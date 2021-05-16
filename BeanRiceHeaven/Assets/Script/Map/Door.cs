5c31e1159e3dc15a0030da816bc96cb144dae23e
KWANMIN LEE <rhksals7864@gmail.com>
2021-05-16 20:45:09 +0900

    add door

--- BeanRiceHeaven/Assets/Script/Map/Door.cs
+++ BeanRiceHeaven/Assets/Script/Map/Door.cs
@@ -6,23 +6,26 @@ using DG.Tweening;
 public class Door : MonoBehaviour
 {
     [SerializeField]
-    Transform targetPoint;
+    float secForOpenning = 1.0f;
     Vector3 Origin;
     bool Opening;
     bool moving;
+
+    float original_scale;
 
     private void Start()
     {
         Opening = false;
         moving = false;
         Origin = transform.position;
+        original_scale = transform.localScale.y;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (!Opening && !moving)
         {
-            transform.DOMove(targetPoint.position, 0.5f);
+            transform.DOScaleY(0.0f, secForOpenning);
             Invoke("CloseDoor", 2.5f);
             Opening = true;
             moving = true;
@@ -31,8 +34,8 @@ public class Door : MonoBehaviour
 
     void CloseDoor()
     {
-        transform.DOMove(Origin, 0.5f);
-        Invoke("MoveEnd", 0.6f);
+        transform.DOScaleY(original_scale, secForOpenning);
+        Invoke("MoveEnd", secForOpenning);
     }
 
     void MoveEnd()
