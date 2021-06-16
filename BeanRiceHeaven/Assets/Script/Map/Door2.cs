4a200251aa0d6b393805e7058ae1cf9c4b92b28f
KWANMIN LEE <rhksals7864@gmail.com>
2021-06-16 20:50:12 +0900

    add map2

---
+++ BeanRiceHeaven/Assets/Script/Map/Door2.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Door2 : MonoBehaviour
+{
+    Transform RightDoor;
+    Transform LeftDoor;
+
+    Vector3 RightOrigin;
+    Vector3 LeftOrigin;
+    float secForOpenning = 1.0f;
+    bool Opening;
+    bool moving;
+
+    void Awake(){
+        RightDoor = transform.Find("Box060");
+        RightOrigin = RightDoor.position;
+        LeftDoor = transform.Find("Box061");
+        LeftOrigin = LeftDoor.position;
+
+        Opening = false;
+        moving = false;
+    }
+    
+    void Start(){
+        RightDoor.name = "right door";
+        LeftDoor.name = "left door";
+    }
+
+    void OnTriggerEnter(Collider collider){
+        if (!Opening && !moving)
+        {
+            RightDoor.DOScaleX(0.0f, secForOpenning);
+            LeftDoor.DOScaleX(0.0f, secForOpenning);
+            Invoke("CloseDoor", 2.5f);
+            Opening = true;
+            moving = true;
+        }
+    }
+
+    void CloseDoor()
+    {
+        RightDoor.DOScaleX(1.0f, secForOpenning);
+        LeftDoor.DOScaleX(1.0f, secForOpenning);
+        Invoke("MoveEnd", secForOpenning + 0.1f);
+    }
+    void MoveEnd()
+    {
+        Opening = false;
+        moving = false;
+    }
+}
