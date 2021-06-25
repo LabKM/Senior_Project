ff836cdfb5ced5382edea100ffba2113a246926b
KWANMIN LEE <rhksals7864@gmail.com>
2021-06-25 22:32:20 +0900

    방 생성할 때 가구도 생성하도록 추가

--- BeanRiceHeaven/Assets/Script/Map/MapGenerator.cs
+++ BeanRiceHeaven/Assets/Script/Map/MapGenerator.cs
@@ -10,11 +10,13 @@ public class MapGenerator : MonoBehaviour
     public Vector2Int wholeMapSize;
     [Range(0, 1)]
     public float RateOfRoom;
-
+    [Range(0, 1)]
+    public float ClosureRate;
     private Vector3 StartPosition;
 
     public GameObject TilePrefab;
     public GameObject RoomPrefab;
+    public GameObject Door2Prefab;
 
     List<Coord> allTileCoords;
     Queue<Coord> shuffledTileCoords;
@@ -28,7 +30,6 @@ public class MapGenerator : MonoBehaviour
     //[HideInInspector]
     //public Transform[,] mapArray { set; get; }
 
-
     // 규칙 
     // 1. 방의 위치는 정 가운데를 기준으로 한다 
     // 2. 방은 최소 1개 (시작지점)을 간디ㅏ
@@ -40,8 +41,116 @@ public class MapGenerator : MonoBehaviour
     // 생성 방식
     // 1. 방은 3x3 정사각형 복도는 2x3 크기로 가정하고 
 
+    private bool ClosedMap(bool[,] map, int i, int j){
+        // 4면이 모두 true인지 확인
+        bool result = true;
+        for (int x = -1; x <= 1; ++x)
+        {
+            for (int y = -1; y <= 1; ++ y)
+            {
+                if ((x==0 || y == 0) && (x != 0 || y  != 0))
+                {   
+                    int neighbourX = i + x;
+                    int neighbourY = j + y;
+                    result = result & map[neighbourX, neighbourY];
+                }
+            }
+        }
+        return result;
+    }
+
+    public bool[,] SetMapFlag(bool[,] unassessableFlag){
+        // 방의 형태 결정
+        // wholeMapSize의 x2 + 1짜리 공간 생성
+        int unassessableTile = 0;
+
+        Queue<Coord> way = new Queue<Coord>();
+
+        // 전부 뚫린 형태로 일단 생성하고 
+        for(int i = 0; i < unassessableFlag.GetLength(0); ++i){
+            for(int j = 0; j < unassessableFlag.GetLength(1); ++j){
+                if( i == 0 || j == 0 || i == unassessableFlag.GetLength(0) - 1 || j == unassessableFlag.GetLength(1) - 1
+                 || ( i % 2 == 0 && j % 2 == 0 ) ){
+                    // 외곽 처리 및 대각선 처리
+                    unassessableTile++;
+                    unassessableFlag[i,j] = true;
+                }else if( i % 2 == 1 && j % 2 == 1) {
+                    unassessableFlag[i,j] = false; // room
+                }else{
+                    unassessableFlag[i,j] = false; // way
+                    way.Enqueue(new Coord(i, j)); 
+                }
+            }
+        }
+
+        int time = 0;
+        // 랜덤으로 타일을 순서대로 뽑아서 길박는 걸 실행
+        Queue<Coord> shuffledWay = new Queue<Coord>(MapUtility.ShuffleArr<Coord>(way.ToArray(), seed));
+        int numOfway = (int)(shuffledWay.Count * ClosureRate);
+        for(int i = 0; i < numOfway; ++i){
+            Coord randomeWayCoord = shuffledWay.Dequeue();
+            unassessableFlag[randomeWayCoord.x, randomeWayCoord.y] = true;
+            unassessableTile++;
+            // 길이 막힌 것으로 인해 인접한 2방의 4면을 모두 막힌 건지 확인
+            // x짝수, y홀수 = 수평 이동길 // x홀수, y짝수 = 수직 이동길
+            if(randomeWayCoord.x % 2 == 0) { // 수평
+                if(ClosedMap(unassessableFlag, randomeWayCoord.x - 1, randomeWayCoord.y)){
+                    unassessableFlag[randomeWayCoord.x - 1, randomeWayCoord.y] = true;
+                    unassessableTile++;
+                }
+                if(ClosedMap(unassessableFlag, randomeWayCoord.x + 1, randomeWayCoord.y)){
+                    unassessableFlag[randomeWayCoord.x + 1, randomeWayCoord.y] = true;
+                    unassessableTile++;
+                }
+            }else{ // 수직
+                if(ClosedMap(unassessableFlag, randomeWayCoord.x, randomeWayCoord.y - 1)){
+                    unassessableFlag[randomeWayCoord.x, randomeWayCoord.y - 1] = true;
+                    unassessableTile++;
+                }
+                if(ClosedMap(unassessableFlag, randomeWayCoord.x, randomeWayCoord.y + 1)){
+                    unassessableFlag[randomeWayCoord.x, randomeWayCoord.y + 1] = true;
+                    unassessableTile++;
+                }
+            }
+            if(!MapIsFullyAccessible(unassessableFlag, unassessableTile, MapCenter * 2 + new Vector2Int(1, 1))){
+                if(randomeWayCoord.x % 2 == 0) { // 수평
+                    if(ClosedMap(unassessableFlag, randomeWayCoord.x - 1, randomeWayCoord.y)){
+                        unassessableFlag[randomeWayCoord.x - 1, randomeWayCoord.y] = false;
+                        unassessableTile--;
+                    }
+                    if(ClosedMap(unassessableFlag, randomeWayCoord.x + 1, randomeWayCoord.y)){
+                        unassessableFlag[randomeWayCoord.x + 1, randomeWayCoord.y] = false;
+                        unassessableTile--;
+                    }
+                }else{ // 수직
+                    if(ClosedMap(unassessableFlag, randomeWayCoord.x, randomeWayCoord.y - 1)){
+                        unassessableFlag[randomeWayCoord.x, randomeWayCoord.y - 1] = false;
+                        unassessableTile--;
+                    }
+                    if(ClosedMap(unassessableFlag, randomeWayCoord.x, randomeWayCoord.y + 1)){
+                        unassessableFlag[randomeWayCoord.x, randomeWayCoord.y + 1] = false;
+                        unassessableTile--;
+                    }
+                }
+                i--;
+                unassessableFlag[randomeWayCoord.x, randomeWayCoord.y] = false;
+                unassessableTile--;
+                time++;
+                shuffledWay.Enqueue(randomeWayCoord);
+            }else{
+                time = 0;
+            }
+            if(time == numOfway){
+                i = numOfway;
+            }
+         }
+
+         return unassessableFlag;
+    }
+
     public void GenerateMap()
     {
+        
         ShuffleCoord();
 
         string holderName = "Generated Map";
@@ -49,30 +158,64 @@ public class MapGenerator : MonoBehaviour
         {
             DestroyImmediate(transform.Find(holderName).gameObject);
         }
-
+        //생성 준비
         GameObject MapHolder = new GameObject(holderName);
         MapHolder.transform.parent = this.transform;
         StartPosition = new Vector3(-wholeMapSize.x / 2 * TileSize.x, 0, wholeMapSize.y / 2 * TileSize.z);
         MapCenter = new Coord(wholeMapSize.x / 2, wholeMapSize.y / 2);
 
+        // 방 담을 공간 및 방의 개수  : 복도의 개수
         Room[,] roomMap = new Room[wholeMapSize.x, wholeMapSize.y];
-
         int NumOfRoom = (int)(RateOfRoom * wholeMapSize.x * wholeMapSize.y);
+
+        // 방의 형태 결정
+        // wholeMapSize의 x2 + 1짜리 공간 생성
+        bool[,] unassessableFlag = new bool[wholeMapSize.x * 2 + 1, wholeMapSize.y * 2 + 1];;
+        unassessableFlag = SetMapFlag(unassessableFlag);
 
         for(int  i = 0; i < NumOfRoom; ++i){
             Coord randomCoord = GetRandomCoord();
             if(roomMap[randomCoord.x, randomCoord.y] != null){ // roomMap이 뭔가로 채워진 경우 다시 
                 i--;
-            }else{ // 방 인스턴스화 하기
-                Vector3 obstaclePos = CoordToVector(randomCoord.x, randomCoord.y);
-                GameObject newObstacle = Instantiate<GameObject>(RoomPrefab, obstaclePos, Quaternion.identity);
-                roomMap[randomCoord.x, randomCoord.y] = newObstacle.GetComponent<Room>();
-                roomMap[randomCoord.x, randomCoord.y].SetDoorStyle(false, false, false, false);
-                newObstacle.transform.parent = MapHolder.transform;
-                newObstacle.transform.name = MapUtility.getRoomName(randomCoord.x, randomCoord.y);
+            }else{ // 방 인스턴스화 하기 
+                Coord wayCenter = randomCoord * 2 + new Vector2Int(1, 1);
+                if( !ClosedMap(unassessableFlag, wayCenter.x, wayCenter.y) ){
+                    Vector3 obstaclePos = CoordToVector(randomCoord.x, randomCoord.y);
+                    GameObject newObstacle = Instantiate<GameObject>(RoomPrefab, obstaclePos, Quaternion.identity);
+                    roomMap[randomCoord.x, randomCoord.y] = newObstacle.GetComponent<Room>();
+                    roomMap[randomCoord.x, randomCoord.y].SetDoorStyle(
+                        !unassessableFlag[wayCenter.x + 1, wayCenter.y], 
+                        !unassessableFlag[wayCenter.x - 1, wayCenter.y],
+                        !unassessableFlag[wayCenter.x, wayCenter.y + 1],
+                        !unassessableFlag[wayCenter.x, wayCenter.y - 1]);
+                    newObstacle.transform.parent = MapHolder.transform;
+                    newObstacle.transform.name = MapUtility.getRoomName(randomCoord.x, randomCoord.y);
+                }
+            }
+        } // Room
+
+        Door2[,] doors_h = new Door2[wholeMapSize.x - 1, wholeMapSize.y];
+        Door2[,] doors_v = new Door2[wholeMapSize.x, wholeMapSize.y - 1];
+        for(int i = 0; i < doors_h.GetLength(0); ++i){
+            for(int j = 0; j < doors_h.GetLength(1); ++j){
+                if((roomMap[i,j] != null || roomMap[i+1, j]) && !unassessableFlag[i*2+2, j*2+1]){
+                    Vector3 point = (CoordToVector(i, j) + CoordToVector(i+1, j)) / 2;
+                    GameObject door = Instantiate<GameObject>(Door2Prefab, point, Quaternion.Euler(0, 90, 0));
+                    doors_h[i,j] = door.GetComponent<Door2>();
+                    door.transform.parent = MapHolder.transform;
+                }
             }
         }
-        
+        for(int i = 0; i < doors_v.GetLength(0); ++i){
+            for(int j = 0; j < doors_v.GetLength(1); ++j){
+                if((roomMap[i,j] != null || roomMap[i, j+1]) && !unassessableFlag[i*2+1, j*2+2]){    
+                    Vector3 point = (CoordToVector(i, j) + CoordToVector(i, j+1)) / 2;
+                    GameObject door = Instantiate<GameObject>(Door2Prefab, point, Quaternion.identity);
+                    doors_v[i,j] = door.GetComponent<Door2>();
+                    door.transform.parent = MapHolder.transform;
+                }
+            }
+        } // Door
 
         for (int i = 0; i < wholeMapSize.x; ++i)
         {
@@ -87,15 +230,17 @@ public class MapGenerator : MonoBehaviour
                     roomMap[i, j] = newTile;
                 }
             }
-        } // Tile
+        } // Floor
     }
 
-    bool MapIsFullyAccessible(bool[,] obstacleMap, int currentObstacleCount)
+    bool MapIsFullyAccessible(bool[,] obstacleMap, int currentObstacleCount, Vector2Int Start)
     {
         bool[,] mapFlag = new bool[obstacleMap.GetLength(0), obstacleMap.GetLength(1)];
+
+        //
         Queue<Coord> queue = new Queue<Coord>();
-        queue.Enqueue(MapCenter);
-        mapFlag[MapCenter.x, MapCenter.y] = true;
+        queue.Enqueue(Start);
+        mapFlag[Start.x, Start.y] = true;
         int accessibleTileCount = 1;
 
         while(queue.Count > 0)
@@ -122,8 +267,39 @@ public class MapGenerator : MonoBehaviour
                 }
             }
         }
-        int targetAccessibleTileCount = wholeMapSize.x * wholeMapSize.y - currentObstacleCount;
+        int targetAccessibleTileCount = obstacleMap.GetLength(0) * obstacleMap.GetLength(1) - currentObstacleCount;
         return targetAccessibleTileCount == accessibleTileCount;
+    }
+
+    void SetRoomDoorDefault(Room[,] roomMap, int i, int j){
+        if(!roomMap[i, j]){
+            return;
+        }
+        if(i == 0 ){
+            if( j == 0 ){
+                roomMap[i,j].SetDoorStyle(true, false, true, false);
+            }else if (j == wholeMapSize.y - 1){
+                roomMap[i,j].SetDoorStyle(true, false, false, true);
+            }else{
+                roomMap[i,j].SetDoorStyle(true, false, true, true);
+            }
+        }else if(i == wholeMapSize.x - 1){
+            if( j == 0 ){
+                roomMap[i,j].SetDoorStyle(false, true, true, false);
+            }else if (j == wholeMapSize.y - 1){
+                roomMap[i,j].SetDoorStyle(false, true, false, true);
+            }else{
+                roomMap[i,j].SetDoorStyle(false, true, true, true);
+            }
+        }else{
+            if( j == 0 ){
+                roomMap[i,j].SetDoorStyle(true, true, true, false);
+            }else if (j == wholeMapSize.y - 1){
+                roomMap[i,j].SetDoorStyle(true, true, false, true);
+            }else{
+                roomMap[i,j].SetDoorStyle(true, true, true, true); 
+            }
+        }
     }
 
     public void ShuffleCoord()
